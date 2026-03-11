1c808dd99760810d92bb0d78669344b79b8a5d6f
alamin2811 <alaminbinmonsur@gmail.com>
2026-03-11 16:40:56 +0600

    modified

--- assets/js/gsap-custom.js
+++ assets/js/gsap-custom.js
@@ -12,7 +12,7 @@
         ScrollSmoother.create({
             wrapper: "#smooth-wrapper",
             content: "#smooth-content",
-            smooth: 1.5,
+            smooth: 1,
             effects: true
         });
 
@@ -47,27 +47,46 @@
             }
         });
     }
+    
+    gsap.registerPlugin(ScrollTrigger);
 
-    $(document).ready(function () {
-        var FundingAcrossParallax = $(".funding-across-images");
-        if (FundingAcrossParallax.length) {
-            // Make sure GSAP and ScrollTrigger are loaded
-            gsap.registerPlugin(ScrollTrigger);
+    const section = document.querySelector(".funding-across-section");
+    const images = document.querySelector(".funding-across-images");
+    const content = document.querySelector(".funding-across-content");
+    
+    if(section && images && content){
+    
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        endTrigger: content,
+        end: "top top",
+        pin: images,
+        pinSpacing: false
+      });
+    
+    }
 
-            // select the parent divs, not the img directly
-            gsap.utils.toArray(".funding-across-images img").forEach((div, i) => {
-                gsap.to(div, {
-                    y: -(i + 1) * 50, // different speed per layer
-                    ease: "none",
-                    scrollTrigger: {
-                        trigger: ".funding-across-section",
-                        start: "top bottom",
-                        end: "bottom top",
-                        scrub: true
-                    }
-                });
-            });
-        }
-    });
+    // $(document).ready(function () {
+    //     var FundingAcrossParallax = $(".funding-across-images");
+    //     if (FundingAcrossParallax.length) {
+    //         // Make sure GSAP and ScrollTrigger are loaded
+    //         gsap.registerPlugin(ScrollTrigger);
+
+    //         // select the parent divs, not the img directly
+    //         gsap.utils.toArray(".funding-across-images img").forEach((div, i) => {
+    //             gsap.to(div, {
+    //                 y: -(i + 1) * 50, // different speed per layer
+    //                 ease: "none",
+    //                 scrollTrigger: {
+    //                     trigger: ".funding-across-section",
+    //                     start: "top bottom",
+    //                     end: "bottom top",
+    //                     scrub: true
+    //                 }
+    //             });
+    //         });
+    //     }
+    // });
 
 })(jQuery);
